318e747de1485fa86867bd6e463baa34f7e41b5a
devm077 <146083735+devm077@users.noreply.github.com>
2024-01-20 16:26:33 +0530

    commit 0

---
+++ script.js
@@ -0,0 +1,9 @@
+// Menu highlighting functionality
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.forEach(link => link.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
